b50b33c59c3a713190e28874d749eaad8312df04
thuyvt <thuyvt1005@gmail.com>
2023-03-22 14:56:13 +0700

    chữa bài lab

---
+++ src/Day8/BaiLab.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Day8;
+
+/**
+ *
+ * @author ThuyVT
+ */
+public class BaiLab {
+    public void bai1(){
+        
+    }
+    
+    public void bai2() {
+        
+    }
+    
+    public void bai3() {
+        // Nhập và xuất danh sách đối tượng
+        QuanLyGiaoVien ql = new QuanLyGiaoVien();
+        ql.nhap();
+        ql.xuat();
+    }
+}
--- src/Day8/QuanLyGiaoVien.java
+++ src/Day8/QuanLyGiaoVien.java
@@ -5,10 +5,49 @@
  */
 package Day8;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ThuyVT
  */
 public class QuanLyGiaoVien {
-    
+
+    // khai báo mảng
+    GiaoVien[] mangGv;
+    Scanner sc = new Scanner(System.in);
+
+    // nhập
+    public void nhap() {
+        System.out.println("Nhập số lượng giảng viên;");
+        int n = Integer.parseInt(sc.nextLine());
+        // Tạo mảng có n giảng viên
+        mangGv = new GiaoVien[n];
+        for (int i = 0; i < n; i++) {
+            GiaoVien gv = new GiaoVien();
+            System.out.println("Nhập mã:");
+            gv.setMaGv(sc.nextLine());
+            System.out.println("Nhập họ và tên:");
+            gv.setHoTen(sc.nextLine());
+            System.out.println("Nhập điểm:");
+            gv.setDiem(Double.parseDouble(sc.nextLine()));
+
+            // gán đối tượng gv vào mảng tại vị trí đang xét
+            mangGv[i] = gv;
+
+        }
+    }
+
+    // xuất
+    public void xuat() {
+        for (GiaoVien item : mangGv) {
+            System.out.println("================");
+            System.out.println("Tên:" + item.getHoTen());
+            System.out.println("Mã gv:" + item.getMaGv());
+            System.out.println("Điểm:" + item.getDiem());
+            System.out.println("Xếp loại:" + item.xepLoai());
+            System.out.println("================");
+        }
+    }
+
 }
